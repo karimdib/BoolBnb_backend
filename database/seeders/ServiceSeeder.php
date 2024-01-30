0919dc09bbe67b70366fed982fa8a05a8e45bbff
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-30 18:45:10 +0100

    Add - migration and seeder for services

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,54 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $services = [
+            [
+                'name' => 'wifi'
+            ],
+            [
+                'name' => 'pool'
+            ],
+            [
+                'name' => 'parking'
+            ],
+            [
+                'name' => 'gym'
+            ],
+            [
+                'name' => 'security'
+            ],
+            [
+                'name' => 'laundry'
+            ],
+            [
+                'name' => 'pets'
+            ],
+            [
+                'name' => 'clubhouse'
+            ],
+            [
+                'name' => 'maintenance'
+            ],
+            [
+                'name' => 'concierge'
+            ]
+        ];
+
+        foreach ($services as $service) {
+            $new_service = new Service;
+            $new_service->name = $service['name'];
+
+            $new_service->save();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
