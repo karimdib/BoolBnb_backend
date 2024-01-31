1623f818b13ae584af6ac7fe90decc43adb82727
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-31 12:04:17 +0100

    add - old value in edit form

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -8,27 +8,27 @@
         @method('PUT')
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control" id="description" placeholder="description" name="description">
+            <input type="text" class="form-control" id="description" name="description" value="{{old('description', $apartment->description)}}">
         </div>
         <div class="mb-3">
             <label for="rooms" class="form-label">Rooms</label>
-            <input type="text" class="form-control" id="last_name" placeholder="rooms" name="rooms">
+            <input type="text" class="form-control" id="last_name" name="rooms" value="{{old('rooms', $apartment->rooms)}}">
         </div>
         <div class="mb-3">
             <label for="beds" class="form-label">Beds</label>
-            <input type="text" class="form-control" id="beds" placeholder="beds" name="beds">
+            <input type="text" class="form-control" id="beds" name="beds" value="{{old('beds', $apartment->beds)}}">
         </div>
         <div class="mb-3">
             <label for="bathrooms" class="form-label">bathrooms</label>
-            <input type="text" class="form-control" id="bathrooms" placeholder="bathrooms" name="bathrooms">
+            <input type="text" class="form-control" id="bathrooms" name="bathrooms" value="{{old('bathrooms', $apartment->bathrooms)}}">
         </div>
         <div class="mb-3">
             <label for="square_meters" class="form-label">square_meters</label>
-            <input type="text" class="form-control" id="square_meters" placeholder="square_meters" name="square_metres">
+            <input type="text" class="form-control" id="square_meters" name="square_meters" value="{{old('square_meters', $apartment->square_meters)}}">
         </div>
         <div class="mb-3">
             <label for="address" class="form-label">address</label>
-            <input type="text" class="form-control" id="address" placeholder="address" name="address">
+            <input type="text" class="form-control" id="address" name="address" value="{{old('address', $apartment->address)}}">
         </div>
         <button type="submit">Edit</button>
     </form>
