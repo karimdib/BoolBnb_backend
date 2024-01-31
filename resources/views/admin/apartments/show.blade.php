5dc98ebd7c11cf182ef987a299e31be0f991cb15
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-31 12:37:14 +0100

    change - show view

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <ul class="container">
+        <li>{{ $apartment->description }}</li>
+        <li>{{ $apartment->rooms }}</li>
+        <li>{{ $apartment->beds }}</li>
+        <li>{{ $apartment->bathrooms }}</li>
+        <li>{{ $apartment->square_meters }}</li>
+        <li>{{ $apartment->address }}</li>
+    </ul>
+
     <div class="container">
-        <p>{{ $apartment->description }}</p>
+        <p>
+            <a href="{{route('admin.apartments.edit',$apartment)}}">Edit</a>
+        </p>
+        <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST" id="deletionForm">
+            @csrf
+            @method('DELETE')
+            <button id="deletion" type="submit" name="{{ $apartment}}">Delete</button>
+        </form>
     </div>
+
 @endsection
