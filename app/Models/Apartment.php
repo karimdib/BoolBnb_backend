4e9fb7d71085975f3ee8e108f6d2f66d10212bbc
karimdib <karim.dib2001@gmail.com>
2024-01-31 17:58:50 +0100

    fix-message()

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -18,6 +18,6 @@ class Apartment extends Model
     ];
     public function messages()
     {
-        return $this->belongsToMany(Message::class);
+        return $this->hasMany(Message::class);
     }
 }
