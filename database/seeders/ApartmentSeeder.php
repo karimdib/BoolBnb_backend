86331eee7b451302abdb9ab436a682cc3a6be5b9
aSwanting <gab.damico@gmail.com>
2024-02-02 21:38:42 +0100

    add: chunk, dashboard testing

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -23,31 +23,32 @@ class ApartmentSeeder extends Seeder
         $apartments = json_decode($json);
         foreach ($apartments as $id => $apartment) {
 
+            // Create, name and assign to user
             $new_apartment = new Apartment();
             $new_apartment->description = $faker->sentence(3);
             $new_apartment->slug = Str::slug($new_apartment->description);
+            $new_apartment->user_id = User::all()->random()->id;
 
+            // Generate coherent rooms, beds, bathrooms, square meters
             $new_apartment->rooms = $faker->numberBetween(1, 12);
             if ($new_apartment->rooms <= 4) {
-
                 $new_apartment->beds = $faker->numberBetween(1, 2);
                 $new_apartment->bathrooms = 1;
                 $new_apartment->square_meters = $faker->numberBetween(50, 80);
             } elseif ($new_apartment->rooms > 4 && $new_apartment->rooms <= 8) {
-
                 $new_apartment->bathrooms = 2;
                 $new_apartment->beds = $faker->numberBetween(3, 4);
                 $new_apartment->square_meters = $faker->numberBetween(81, 120);
             } else {
-
                 $new_apartment->bathrooms = 3;
                 $new_apartment->beds = $faker->numberBetween(5, 6);
                 $new_apartment->square_meters = $faker->numberBetween(121, 200);
             }
 
+            // Get latitude and longitude from json
             $new_apartment->latitude = $apartment->position->lat;
             $new_apartment->longitude = $apartment->position->lon;
-            $new_apartment->user_id = User::all()->random()->id;
+
             $new_apartment->save();
         }
     }
