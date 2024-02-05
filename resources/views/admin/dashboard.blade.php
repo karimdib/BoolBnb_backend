692f41efd22841809130dea47c7dfda28ec3c00d
Nicola Tabai <nicola.tabai98@gmail.com>
2024-02-05 17:20:07 +0100

    Add - index function in dashboard controller Fix - route redirecting on dashboard Add - tables on dashboard view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,120 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<section class="container card py-3 my-3">
+    <div class="d-flex justify-content-between">
+        <h1 class="card-title">Your Apartments</h1>
+        <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary align-self-center">View All</a>
     </div>
-</div>
+    <table class="card-body">
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Apartment ID</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($apartments as $apartment)
+            <tr>
+                {{-- database fields --}}
+                <td>{{ $apartment->user_id }} </td>
+                <td>{{ $apartment->id }} </td>
+        
+                <td>
+                    <a href="{{ route('admin.apartments.show', $apartment) }}">
+                        {{ $apartment->description }}
+                    </a>
+                </td>
+        
+                <td>{{ $apartment->address }} </td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="5">
+                    <p class="fs-4 text-center p-4 opacity-75">
+                        No Apartments...
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+            @endforelse
+    </table>
+</section>
+<section class="container card py-3 mb-3">
+    <div class="d-flex justify-content-between">
+        <h1 class="card-title">Your Sponsorships</h1>
+        {{-- creare una rotta per lo storico degli ordini --}}
+        <a href="" class="btn btn-primary align-self-center">View All</a>
+    </div>
+    <table>
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($orders as $order)
+        <tr>
+            {{-- database fields --}}
+            {{-- <td>{{ $order->date_start }} </td>
+            <td>{{ $order->date_end }} </td>
+    
+            <td>
+                
+            </td>
+    
+            <td>{{ $apartment->address }} </td> --}}
+        </tr>
+        @empty
+        <tr>
+            <td colspan="5">
+                <p class="fs-4 text-center p-4 opacity-75">
+                    No Sponsorships
+                </p>
+            </td>
+        </tr>
+    </tbody>
+        @endforelse
+</table>
+</section>
+<section class="container card py-3 mb-3">
+    <div class="d-flex justify-content-between">
+        <h1 class="card-title">Statistics</h1>
+        {{-- creare una rotta per le statistiche --}}
+        <a href="" class="btn btn-primary align-self-center">View All</a>
+    </div>
+    <table>
+    <thead>
+        <tr>
+            
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($orders as $order)
+        <tr>
+            
+        </tr>
+        @empty
+        <tr>
+            <td colspan="5">
+                <p class="fs-4 text-center p-4 opacity-75">
+                    No Sponsorships
+                </p>
+            </td>
+        </tr>
+    </tbody>
+        @endforelse
+</table>
+</section>
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('admin.dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
