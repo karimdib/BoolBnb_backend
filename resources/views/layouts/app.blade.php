141877d2d0ba68f2e199ad8003202f2daf5ded29
ValerioCarbone <valeriocarbone10@gmail.com>
2024-02-06 15:27:10 +0100

    add - dropdown anchor to apartments index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                                    <a class="dropdown-item"
+                                        href="{{ route('admin.apartments.index') }}">{{ __('Apartments') }}</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
