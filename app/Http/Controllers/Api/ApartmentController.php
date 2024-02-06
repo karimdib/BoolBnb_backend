70595e90e9a2243abbabc7fde54915d8560cc762
aSwanting <gab.damico@gmail.com>
2024-02-06 20:54:43 +0100

    add: api post route, api controller filter method

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -10,7 +10,6 @@ class ApartmentController extends Controller
 {
     public function index(Request $request)
     {
-        // $results = Apartment::with('services')->limit(20);
         $results = Apartment::with('user', 'services')->get();
 
         return response()->json([
@@ -25,6 +24,27 @@ class ApartmentController extends Controller
 
         return response()->json([
             'apartment' => $apartment
+        ]);
+    }
+
+    public function filter()
+    {
+        $data = request();
+
+        $results = Apartment::with('user', 'services')
+            ->selectRaw('*, ROUND(ST_DISTANCE_SPHERE(POINT(longitude, latitude), POINT(' . $data->longitude . ',  ' . $data->latitude . ')) / 1000, 2) AS distance')
+            ->havingRaw('distance <= ' . $data->search_radius)
+            ->where([
+                ['rooms', '>=', $data->rooms],
+                ['beds', '>=', $data->beds],
+                ['bathrooms', '>=', $data->bathrooms],
+                ['square_meters', '>=', $data->square_meters],
+            ])->orderBy('distance')
+            ->get();
+
+        return response()->json([
+            'results' => $results,
+            'success' => true
         ]);
     }
 }
