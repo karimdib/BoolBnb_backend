9b06a0f15c3944361f28a576e35b534f4b758abb
karimdib <karim.dib2001@gmail.com>
2024-02-07 16:18:18 +0100

    translate error validation ita to eng

--- public/js/apartmentValidation.js
+++ public/js/apartmentValidation.js
@@ -10,7 +10,7 @@ const form = document.querySelector('.apartment-form');
 const service = document.querySelectorAll('.service');
 const visible = document.getElementById('visible');
 const invisible = document.getElementById('invisible');
-const radioButtons = document.getElementById('radio-buttons'); 
+const radioButtons = document.getElementById('radio-buttons');
 const isInvalid = document.getElementById('service-error');
 console.log(isInvalid);
 console.log(service);
@@ -26,7 +26,7 @@ form.addEventListener('submit', function (event) {
         }
     })
     if (!serviceChecked) {
-        isInvalid.innerText = 'Scegli almeno un servizio';
+        isInvalid.innerText = 'Choose at least one service';
         console.log('nessun sevizio selezionato');
         service.forEach((input) => {
             input.classList.add('is-invalid');
@@ -46,13 +46,13 @@ form.addEventListener('submit', function (event) {
         visible.classList.add('is-invalid');
         invisible.classList.add('is-invalid');
         radioButtons.classList.add('is-invalid');
-        radioButtons.nextElementSibling.innerText = 'Seleziona almeno un opzione';
+        radioButtons.nextElementSibling.innerText = 'Select at least one option';
     }
 
     const validateNumber = (inputElement, fieldName) => {
         const value = parseFloat(inputElement.value);
         if (isNaN(value) || value <= 0) {
-            inputElement.nextElementSibling.innerText = `Il campo ${fieldName} deve essere un numero maggiore di 0.`;
+            inputElement.nextElementSibling.innerText = `The ${fieldName} field must be a number greater than 0.`;
             isValid = false;
         } else {
             inputElement.nextElementSibling.innerText = '';
@@ -62,17 +62,17 @@ form.addEventListener('submit', function (event) {
     validateNumber(rooms, 'rooms');
     validateNumber(beds, 'beds');
     validateNumber(bathrooms, 'bathrooms');
-    validateNumber(squareMeters, 'square_metres');
+    validateNumber(squareMeters, 'square metres');
 
     if (nameTitle.value.trim() === '') {
-        nameTitle.nextElementSibling.innerText = 'Il campo name non può essere vuoto.';
+        nameTitle.nextElementSibling.innerText = 'The name field cannot be empty.';
         isValid = false;
     } else {
         nameTitle.nextElementSibling.innerText = '';
     }
 
     if (description.value.trim() === '') {
-        description.nextElementSibling.innerText = 'Il campo description non può essere vuoto.';
+        description.nextElementSibling.innerText = 'The description field cannot be empty.';
         isValid = false;
     } else {
         description.nextElementSibling.innerText = '';
