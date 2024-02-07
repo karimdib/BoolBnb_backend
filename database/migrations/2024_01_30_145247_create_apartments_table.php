284da67cda0a61fd1ce34ded56fb66f6acb23006
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-07 10:03:35 +0100

    add - images delete in storage on rollback

--- database/migrations/2024_01_30_145247_create_apartments_table.php
+++ database/migrations/2024_01_30_145247_create_apartments_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
 
 return new class extends Migration
 {
@@ -35,6 +36,15 @@ return new class extends Migration
      */
     public function down(): void
     {
+        //QUESTA FUNZIONE FUNZIONA SOLO CON IL ROLLBACK, CON IL MIGRATE:FRESH NO
+        // Otteniamo il percorso della cartella
+        $cover_images_path = 'storage\app\public\cover_images';       
+        // Ottieni l'elenco di tutti i file nella cartella
+        $files = File::allFiles($cover_images_path);
+        // Elimina ciascun file all'interno della cartella
+        File::delete($files);
+
         Schema::dropIfExists('apartments');
-    }
+
+        }
 };
