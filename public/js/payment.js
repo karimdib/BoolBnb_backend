03b60262ebbbf0019e91cbde7509870b616b4aad
karimdib <karim.dib2001@gmail.com>
2024-02-09 18:02:02 +0100

    remove class in payament.js

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,6 +1,4 @@
 console.log('validation JS payament');
-const cardholderNameFather = document.getElementById('card-name-father');
-const numberCardFather = document.getElementById('card-number-father');
 const cardholderName = document.getElementById('cardholder-name');
 const numberCard = document.getElementById('number-card');
 const promo = document.getElementById('promo');
@@ -10,14 +8,12 @@ form.addEventListener('submit', function (event) {
     let finishCheckout = true;
     if (cardholderName.value.trim() === '') {
         cardholderName.nextElementSibling.innerText = 'the name field cannot be empty';
-        cardholderNameFather.classList.add('red');
         cardholderName.classList.add('red')
         finishCheckout = false;
     }
 
     if (numberCard.value.trim() === '') {
         numberCard.nextElementSibling.innerText = 'the number card field cannot be empty';
-        numberCardFather.classList.add('red');
         numberCard.classList.add('red');
         finishCheckout = false;
     }
@@ -26,4 +22,14 @@ form.addEventListener('submit', function (event) {
     if (!finishCheckout) {
         event.preventDefault();
     }
+
+    //rimozionx classe is-invalid, scrivendo nell'input
+    numberCard.addEventListener('input', function () {
+        numberCard.classList.remove('red');
+        numberCard.nextElementSibling.innerText = '';
+    });
+    cardholderName.addEventListener('input', function () {
+        cardholderName.classList.remove('red');
+        cardholderName.nextElementSibling.innerText = '';
+    });
 })
