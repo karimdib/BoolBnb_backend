f8c0e31cb22dc1831cc4a92fa98d1d4bbae38803
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-09 17:29:10 +0100

    add - colors in navbar and in buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,24 +23,15 @@
     <div id="app">
 
 
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    {{-- config('app.name', 'Laravel') --}}
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
                             {{-- <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a> --}}
-                            <a class="nav-link" href="http://localhost:5174/">{{ __('Home') }}</a>
+                            <a class="nav-link" href="http://localhost:5174/">boolbnb</a>
                         </li>
                     </ul>
 
