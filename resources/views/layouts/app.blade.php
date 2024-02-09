d34a996e1816b77adda9ca3f5785c2be6ef42b79
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-09 14:50:02 +0100

    Add - link on Home to front office

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,8 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                            {{-- <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a> --}}
+                            <a class="nav-link" href="http://localhost:5174/">{{ __('Home') }}</a>
                         </li>
                     </ul>
 
