ab26177d9608e76ac087745f97666f5ac1707cc5
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-13 13:26:07 +0100

    working on visits

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\VisitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/apartments', [ApartmentController::class, 'index']);
+// Route::get('/apartments/results', [ApartmentController::class, 'results']);
+// Route::get('/apartments/{apartment:slug}', [ApartmentController::class, 'show']);
+// Route::post('/apartments/search', [ApartmentController::class, 'fuzzySearch']);
+// Route::post('/apartments', [ApartmentController::class, 'filter']);
+
+Route::post('/visit', [VisitController::class],'store');
