047ecc96502638f35dbef71ae224919e0ab3b148
karimdib <karim.dib2001@gmail.com>
2024-02-14 17:19:51 +0100

    uncomment js

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -143,9 +143,11 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script> const visitsJS = @php echo json_encode($visits); @endphp </script>
+<script>
+    const visitsJS = @php echo json_encode($visits); @endphp 
+</script>
 @push('scripts')
-    <script type="module" src="{{ asset('/js/visitsGraph.js') }}"></script>
+<script type="module" src="{{ asset('/js/visitsGraph.js') }}"></script>
 @endpush
 
 <x-delete-modal />
@@ -172,10 +174,10 @@
         width: 70px
     }
 </style>
-{{-- @push('scripts')
+@push('scripts')
 <script src="{{ asset('./js/payment.js') }}"></script>
 @endpush
 @push('scripts')
 <script src="{{ asset('js/deleteModal.js') }}"></script>
-@endpush --}}
+@endpush
 @endsection
