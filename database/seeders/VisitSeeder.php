73a769d4e7c93d3dca9892cfaceeaa0a5433e5c3
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-14 16:45:55 +0100

    fix visit seeder

--- database/seeders/VisitSeeder.php
+++ database/seeders/VisitSeeder.php
@@ -21,8 +21,8 @@ class VisitSeeder extends Seeder
         foreach ($apartments as $apartment) {
             //Genero 20 visite per ogni appartamento nell'ultimo mese
             for ($i=0; $i < 20; $i++) { 
-                // Genero una data casuale tra oggi e un mese fa
-                $startDate = strtotime('-1 month');
+                // Genero una data casuale tra oggi e una settimana fa
+                $startDate = strtotime('-1 week');
                 $endDate = time();
                 $randomTimestamp = rand($startDate, $endDate);
                 // Formatto la data nel formato YY-MM-DD hh:mm:ss
