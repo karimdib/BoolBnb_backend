d70a0ab05bb6a0da6c8cc8b2e8c0a461628d292e
karimdib <karim.dib2001@gmail.com>
2024-02-15 16:34:52 +0100

    import footer-backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,76 @@
             @yield('content')
             @stack('scripts')
         </main>
+        <footer class="ff">
+            <div class="container">
+                <div class="row  justify-content-between flex-wrap  flex-md-nowrap">
+                    <div class="col-md-4 col-6">
+                        <ul>
+                            <a href="#">
+                                <li>Chi Siamo</li>
+                            </a>
+                            <a href="#">
+                                <li>Contatti</li>
+                            </a>
+                            <a href="#">
+                                <li>Pubblica il tuo annuncio</li>
+                            </a>
+                            <a href="#">
+                                <li>Lavora con noi</li>
+                            </a>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-6">
+                        <ul>
+                            <a href="#">
+                                <li>Termini di utilizzo</li>
+                            </a>
+                            <a href="#">
+                                <li>Normativa sulla privacy e sui cookie</li>
+                            </a>
+                            <a href="#">
+                                <li>Appartamenti vicino a me </li>
+                            </a>
+                            <a href="#">
+                                <li>Domande Frequenti</li>
+                            </a>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-12  text-center icon-col ">
+                        <ul>
+                            <a href="#">
+                                <li class="icon">Seguici su:</li>
+                            </a>
+                        </ul>
+                        <ul class="d-flex gap-3  justify-content-center justify-content-md-between p-0">
+                            <a href="#">
+                                <li class="icon">
+                                    <font-awesome-icon :icon="['fab', 'facebook']" />
+                                </li>
+                            </a>
+                            <a href="#">
+                                <li class="icon">
+                                    <font-awesome-icon :icon="['fab', 'instagram']" />
+                                </li>
+                            </a>
+                            <a href="#">
+                                <li class="icon">
+                                    <font-awesome-icon :icon="['fab', 'github']" />
+                                </li>
+                            </a>
+                            <a href="#">
+                                <li class="icon">
+                                    <font-awesome-icon :icon="['fab', 'telegram']" />
+                                </li>
+                            </a>
+                        </ul>
+                    </div>
+                    <!-- <div class="col-12">
+                  <img class="city-image" src="/public/cityy.png" alt="">
+                </div> -->
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 
