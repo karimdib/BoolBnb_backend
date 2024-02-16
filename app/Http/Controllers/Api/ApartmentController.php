7dd0baaf480dda198caf9d759673eda6171b8eb4
ValerioCarbone <valeriocarbone10@gmail.com>
2024-02-16 18:39:48 +0100

    change api apt index limit 5

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -24,7 +24,7 @@ class ApartmentController extends Controller
             // ->orderBy("sponsored")
             ->has('orders')
             ->inRandomOrder()
-            ->limit(12)->get();
+            ->limit(15)->get();
 
 
         return response()->json([
