eeaf2ad7350bac435314545d6124ca4ae280e800
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-18 18:52:55 +0100

    Fix - order seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Sponsorship;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 class OrderSeeder extends Seeder
 {
@@ -23,10 +24,25 @@ class OrderSeeder extends Seeder
         $apartmentIds = $activeApartments->pluck('id');
 
 
-        for ($i = 0; $i < 23; $i++) {
+        for ($i = 0; $i < 50; $i++) {
             $new_order = new Order();
             $new_order->apartment_id = $faker->randomElement($apartmentIds);
             $new_order->sponsorship_id = $faker->randomElement($sponsorIds);
+
+            $startDate = Carbon::now()->subMonth(); // Ottieni la data di un mese fa
+            $endDate = Carbon::now(); // Ottieni la data di oggi            
+            $randomDateTime = $faker->dateTimeBetween($startDate, $endDate);// Genera una data compresa tra $startDate e $endDate
+
+            $new_order->date_start = $randomDateTime->format('Y-m-d H:i:s');
+            $randomDate = Carbon::instance($randomDateTime);
+            if ($new_order->sponsorship_id == 1) { //1 giorno
+                $new_order->date_end = $randomDate->addDay()->format('Y-m-d H:i:s');
+            } else if ($new_order->sponsorship_id == 2) { //2 giorno
+                $new_order->date_end = $randomDate->addDays(2)->format('Y-m-d H:i:s'); 
+            } else { //6 giorno
+                $new_order->date_end = $randomDate->addDays(6)->format('Y-m-d H:i:s');
+            }
+
             $new_order->save();
         }
     }
