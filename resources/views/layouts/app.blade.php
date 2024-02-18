4f8c63c6462eb6eeb4bcc5c813f61152ff8aab6a
EmanueleVenditti95 <vendittiema@gmail.com>
2024-02-18 11:02:07 +0100

    fixed footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,30 +80,29 @@
             </div>
         </nav>
 
-        <main class="">
+        <main class="main">
             @yield('content')
             @stack('scripts')
         </main>
         <footer class="footer">
             <div class="container">
-                <div class="text-center p-2">
-                    <h5 class="title-footer">Boolbnb Team-1 Boolean</h5>
-                    <div class="made-by">Made By:</div>
-                    <div class=" list-creator  mt-4 d-flex justify-content-between  d-none ">
+                <div class="text-center p-2 mb-0">
+                    <p class="title-footer mb-1">BoolBnb made by Team 1, Boolean class 106 :</p>
+                    <div class=" list-creator mt-0 d-flex justify-content-between">
                         <a href="https://github.com/aSwanting" target="_blank" class="name-creator">
-                            <div>Gabriel</div>
+                            <div>Gabriel D'Amico</div>
                         </a>
                         <a href="https://github.com/ValerioCarbone" target="_blank" class="name-creator">
-                            <div>Valerio</div>
+                            <div>Valerio Carbone</div>
                         </a>
                         <a href="https://github.com/EmanueleVenditti95" target="_blank" class="name-creator">
-                            <div>Emenuele</div>
+                            <div>Emanuele Venditti</div>
                         </a>
                         <a href="https://github.com/w3bd3v3lop3rNico" target="_blank" class="name-creator">
-                            <div>Nicola</div>
+                            <div>Nicola Tabai</div>
                         </a>
                         <a href="https://github.com/karimdib" target="_blank" class="name-creator">
-                            <div>Karim</div>
+                            <div>Karim Dib</div>
                         </a>
                     </div>
                 </div>
