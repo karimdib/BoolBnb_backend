71d5a17b5c2263a149801b4ffbcdcfa0f0d8599f
karimdib <karim.dib2001@gmail.com>
2024-02-19 11:53:04 +0100

    fix-status-order.index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -17,34 +17,34 @@
         </thead>
         <tbody>
             @forelse ($apartment_orders as $order)
-                <tr>
-                    <td>
-                        <a href="{{ route('admin.apartments.show', $order->apartment_id) }}">
+            <tr>
+                <td>
+                    <a href="{{ route('admin.apartments.show', $order->apartment_id) }}">
                         {{ $order->apartment->address }}</a>
-                    </td>
-                    @if ($order->sponsorship_id === 1)
-                        <td>Gold</td>
-                    @elseif($order->sponsorship_id === 2)
-                        <td>Diamond</td>
-                    @elseif($order->sponsorship_id === 3)
-                        <td>Platinum</td>
-                    @endif
-                    @if ($date_now <= $order->date_end)
-                        <td class="active status">Active</td>
+                </td>
+                @if ($order->sponsorship_id === 1)
+                <td>Gold</td>
+                @elseif($order->sponsorship_id === 2)
+                <td>Diamond</td>
+                @elseif($order->sponsorship_id === 3)
+                <td>Platinum</td>
+                @endif
+                @if ($date_now <= $order->date_end)
+                    <td class="active status index-status">Active</td>
                     @elseif($date_now >= $order->date_end)
-                        <td class="inactive status">Inactive</td>
+                    <td class="inactive status index-status">Inactive</td>
                     @endif
                     <td class="data-start">{{ $order->date_start }} </td>
                     <td class="data-end">{{ $order->date_end }} </td>
-                </tr>
+            </tr>
             @empty
-                <tr>
-                    <td colspan="5">
-                        <p class="fs-4 text-center p-4 opacity-75">
-                            No Sponsorships
-                        </p>
-                    </td>
-                </tr>
+            <tr>
+                <td colspan="5">
+                    <p class="fs-4 text-center p-4 opacity-75">
+                        No Sponsorships
+                    </p>
+                </td>
+            </tr>
         </tbody>
         @endforelse
     </table>
@@ -53,5 +53,14 @@
 @endsection
 
 <style lang="scss" scoped>
+    .table {
+        border-style: none !important;
+        border-color: white !important;
+    }
 
+    @media screen and (min-width:769px) {
+        .index-status {
+            margin-top: 0px !important;
+        }
+    }
 </style>
